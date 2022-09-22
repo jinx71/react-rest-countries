a370f3c7e0db9f19833fe23035c0d82eafb91738
jinx71 <karim.sazed@gmail.com>
2022-09-22 09:38:22 +0600

    Footer, Contact Form, About Us Added

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is About Page</h1>
+        <div className="p-4 w-50 mx-auto">
+            <h2 style={{ textAlign: "left" }}>About Us!</h2>
+            <h3 style={{ textAlign: "left" }}>Welcome To <span id="W_Name1">React Rest Countries</span></h3>
+            <p style={{ textAlign: "left" }}><span id="W_Name2">React Rest Countries</span> is a Professional <span id="W_Type1">Educational</span> Platform. Here we will provide you only interesting content, which you will like very much. We're dedicated to providing you the best of <span id="W_Type2">Educational site</span>, with a focus on dependability and <span id="W_Spec">React App</span>. We're working to turn our passion for <span id="W_Type3">Educational site</span> into a booming <a href="https://www.blogearns.com/2021/05/free-about-us-page-generator.html" rel="do-follow" style={{ color: "inherit", textDecoration: "none" }}>online website</a>. We hope you enjoy our <span id="W_Type4">Educational site</span> as much as we enjoy offering them to you.</p>
+            <p style={{ textAlign: "left" }}>I will keep posting more important posts on my Website for all of you. Please give your support and love.</p>
+            <p style={{ fontWeight: "bold", textAlign: "center" }}>Thanks For Visiting Our Site <br></br>
+                <span style={{ color: "blue", fontSize: "16px", fontWeight: "bold", textAlign: "center" }}>Have a nice day!</span></p>
         </div>
     );
 };
--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div class="card container-fluid border-0">
+        <div class="card container-fluid border-0 footer">
             <div class="card-body">
                 &copy; Copyright || 2022
             </div>
